267c9ec95c344854d1e3d4c1dc95f5c5b66e2110
alanpereiradasilva <161897310+alanpereiradasilva@users.noreply.github.com>
2025-02-17 16:57:10 -0300

    treinando

--- jogo.py
+++ jogo.py
@@ -1,139 +1,71 @@
 import pygame
 from pygame.locals import *
 from sys import exit
-from random import randint
 
 pygame.init()
 
 largura = 640
 altura = 480
-x_cobra = int(largura/2)
-y_cobra = int(altura/2)
 
-velocidade = 2
-x_controle = velocidade
-y_controle = 0
-
-
-x_maca = randint(40, 600)
-y_maca = randint(50, 430)
-
-pontos = 0
-fonte = pygame.font.SysFont('arial', 40, True, True)
+PRETO = (0,0,0)
 
 tela = pygame.display.set_mode((largura, altura))
-pygame.display.set_caption('Jogo')
+pygame.display.set_caption('Sprites')
+
+class Sapo(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.sprites = []
+        self.sprites.append(pygame.image.load('sprites/attack_1.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_2.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_3.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_4.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_5.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_6.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_7.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_8.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_9.png'))
+        self.sprites.append(pygame.image.load('sprites/attack_10.png'))
+        self.atual = 0
+        self.image = self.sprites[self.atual]
+        self.image = pygame.transform.scale(self.image, (128*3, 64*3))
+
+
+        self.rect = self.image.get_rect()
+        self.rect.topleft = 100, 100
+        self.animar = False
+
+    def atacar(self):
+        self.animar = True
+
+    def update(self):
+        if self.animar == True:
+            self.atual += 0.25
+            if self.atual >= len(self.sprites):
+                self.atual = 0
+                self.animar = False
+            self.image = self.sprites[int(self.atual)]
+            self.image = pygame.transform.scale(self.image, (128*3, 64*3))
+
+
+todas_as_sprites = pygame.sprite.Group()
+sapo = Sapo()
+todas_as_sprites.add(sapo)
+
 relogio = pygame.time.Clock()
-lista_cobra = []
-comprimento_inicial = 5
-morreu = False
-
-def aumentar_cobra(lista_cobra):
-    for XeY in lista_cobra:
-
-        pygame.draw.rect(tela, (0,255,0), (XeY[0], XeY[1], 20, 20))
-
-def reiniciar_jogo():
-    global pontos, comprimento_inicial, x_cobra, y_cobra, lista_cobra, lista_cabeca, x_maca, y_maca, morreu
-    pontos = 0
-    comprimento_inicial = 5
-    x_cobra = int(largura/2)
-    y_cobra = int(altura/2)
-    lista_cobra = []
-    lista_cabeca = []
-    x_maca = randint(40, 600)
-    y_maca = randint(50, 430)
-    morreu = False
 
 
 while True:
-    relogio.tick(120)
-    tela.fill((255,255,255))
-    mensagem = f"Pontos: {pontos}"
-    texto_formatado = fonte.render(mensagem, True, (0,0,0))
+    relogio.tick(30)
+    tela.fill(PRETO)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
-            exit()
-        
+            exit()    
         if event.type == KEYDOWN:
-            if event.key == K_a:
-                if x_controle == velocidade:
-                    pass
-                else:
-                    x_controle = -velocidade
-                    y_controle = 0
-            if event.key == K_d:
-                if x_controle == -velocidade:
-                    pass
-                else:
-                    x_controle = velocidade
-                    y_controle = 0
-            if event.key == K_w:
-                if y_controle == velocidade:
-                    pass
-                else:
-                    y_controle = -velocidade
-                    x_controle = 0
-            if event.key == K_s:
-                if y_controle == -velocidade:
-                    pass
-                else:
-                    y_controle = velocidade 
-                    x_controle = 0
+            sapo.atacar()
+
     
-    x_cobra += x_controle
-    y_cobra += y_controle
-
-    cobra = pygame.draw.rect(tela, (0,255,0), (x_cobra,y_cobra,20,20))
-    maca = pygame.draw.rect(tela, (255,0,0), (x_maca,y_maca,20,20))
-
-    if cobra.colliderect(maca):
-        x_maca = randint(40, 600)
-        y_maca = randint(50, 430)
-        pontos += 1
-        comprimento_inicial += 1
-
-    lista_cabeca = []
-    lista_cabeca.append(x_cobra)
-    lista_cabeca.append(y_cobra)
-
-    lista_cobra.append(lista_cabeca)
-
-    if lista_cobra.count(lista_cabeca) > 1:
-        fonte2 = pygame.font.SysFont('arial', 20, True, True)
-        mensagem = 'Game over! Pressione a tecla R para jogar novamente.'
-        texto_formatado = fonte2.render(mensagem, True, (0,0,0))
-        ret_texto = texto_formatado.get_rect()
-
-        morreu = True
-        while morreu:
-            tela.fill(255, 255, 255)
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    pygame.quit()
-                    exit()
-                if event.type == KEYDOWN:
-                    if event.key == K_r:
-                        reiniciar_jogo()
-            
-            ret_texto.center = (largura//2, altura//2)
-            tela.blit(texto_formatado, ret_texto)
-            pygame.display.update()
-
-    if x_cobra > largura:
-        x_cobra = 0
-    if x_cobra < 0:
-        x_cobra = largura
-    if y_cobra < 0:
-        y_cobra = altura
-    if y_cobra > altura:
-        y_cobra = 0
-
-    if len(lista_cobra) > comprimento_inicial:
-        del lista_cobra[0]
-    aumentar_cobra(lista_cobra)
-
-    tela.blit(texto_formatado, (450,40))
-
-    pygame.display.update()
+    todas_as_sprites.draw(tela)
+    todas_as_sprites.update()
+    pygame.display.flip()
